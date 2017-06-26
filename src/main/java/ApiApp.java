dc9d96e14b215a6f8d8915376595014d5569e0d1
Trevor Davis <tzendou@gmail.com>
2017-06-26 10:39:37 -0700

    start

---
+++ src/main/java/ApiApp.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by tdavis on 6/26/17.
+ */
+@SpringBootApplication
+public class ApiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApp.class, args);
+    }
+}
